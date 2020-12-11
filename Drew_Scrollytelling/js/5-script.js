2570f235f4c775c0e8759f082af54b16e883170b
Paula Weibel <paula.weibel@stud.hslu.ch>
2020-12-11 12:37:20 +0100

    small changes + mail spam

--- Drew_Scrollytelling/js/5-script.js
+++ Drew_Scrollytelling/js/5-script.js
@@ -88,6 +88,25 @@ var updateProgress = function () {
       deadFace.style.opacity = 0;
     }
 
+    // TEXT 2 (noooOOOOOO.)
+    if (percentage >= 80 && percentage < 99) {
+      phrase2.style.display = "block";
+    } else {
+      phrase2.style.display = "none";
+    }
+
+    // JASON DE RULER
+    if (percentage >= 80 && percentage < 99) {
+      phrase2.style.display = "block";
+    } else {
+      phrase2.style.display = "none";
+    }
+
+    // TRIGGER EMAIL
+    if (percentage >= 85 && percentage <= 87) {
+      show_email1();
+    }
+
   })
 }
 updateProgress();
@@ -100,13 +119,13 @@ $(window).scroll(updateProgress);
 // FIRST NOTIFICATION CONTENT
 function show_email1() {
   console.log('works')
-  disableScroll();
+  // disableScroll();
   toast.create({
-    title: 'From: Mr Bosstitch <br> Subject: New Project',
+    title: 'From: Jason de Ruler',
     text: '----------------------------<br> Hi Drew,<br> I have a new task for you, design a logo.<br><br> Cheers,<br> Mr. B',
     icon: "img/button-mail.png",
     callback: function () {
-      enableScroll();
+      // enableScroll();
       // SecondPart();
     }
   });
@@ -166,3 +185,175 @@ new TypeIt("#phrase2", {
   speed: 50,
   waitUntilVisible: true
 }).go();
+
+
+
+
+
+
+// NOTIFICATION SCRIPTS - FOR THE TOAST NOTIFICATION
+
+// FIRST NOTIFICATION CONTENT
+function show_email1() {
+  console.log('works')
+  disableScroll();
+  toast.create({
+    title: 'From: Mr Bosstitch <br> Subject: New Project',
+    text: '----------------------------<br> Hi Drew,<br> I have a new task for you, design a logo.<br><br> Cheers,<br> Mr. B',
+    icon: "img/button-mail.png",
+    callback: function () {
+      enableScroll();
+      // SecondPart();
+    }
+  });
+};
+
+// SECOND NOTIFICATION CONTENT
+function show_email2() {
+  console.log('works')
+  disableScroll();
+  toast.create({
+    title: 'From: Mr Bosstitch <br> Subject: i forgot...',
+    text: '----------------------------<br> Hi Drew,<br>please finish the project by the end of the week.<br><br> Cheers,<br> Mr. B',
+    icon: "img/button-mail.png",
+    callback: function () {
+      enableScroll();
+      SecondPart();
+    }
+
+  });
+};
+
+
+function disableScroll() {
+  // Get the current page scroll position
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+
+    // if any scroll is attempted, set this to the previous value
+    window.onscroll = function () {
+      window.scrollTo(scrollLeft, scrollTop);
+    };
+}
+
+function enableScroll() {
+  window.onscroll = function () {};
+}
+
+function SecondPart() {
+  window.location.replace("part2.html");
+  reload();
+}
+
+
+
+// TOAST SCRIPT
+(function (root, factory) {
+  try {
+    // commonjs
+    if (typeof exports === 'object') {
+      module.exports = factory();
+      // global
+    } else {
+      root.toast = factory();
+    }
+  } catch (error) {
+    console.log('Isomorphic compatibility is not supported at this time for toast.')
+  }
+})(this, function () {
+
+  // We need DOM to be ready
+  if (document.readyState === 'complete') {
+    init();
+  } else {
+    window.addEventListener('DOMContentLoaded', init);
+  }
+
+  // Create toast object
+  toast = {
+    // In case toast creation is attempted before dom has finished loading!
+    create: function () {
+      console.error([
+        'DOM has not finished loading.',
+        '\tInvoke create method when DOM\s readyState is complete'
+      ].join('\n'))
+    }
+  };
+  var autoincrement = 0;
+
+  // Initialize library
+  function init() {
+    // Toast container
+    var container = document.createElement('div');
+    container.id = 'cooltoast-container';
+    document.body.appendChild(container);
+
+    // @Override
+    // Replace create method when DOM has finished loading
+    toast.create = function (options) {
+      var toast = document.createElement('div');
+      toast.id = ++autoincrement;
+      toast.id = 'toast-' + toast.id;
+      toast.className = 'cooltoast-toast';
+
+      // title
+      if (options.title) {
+        var h4 = document.createElement('h4');
+        h4.className = 'cooltoast-title';
+        h4.innerHTML = options.title;
+        toast.appendChild(h4);
+      }
+
+      // text
+      if (options.text) {
+        var p = document.createElement('p');
+        p.className = 'cooltoast-text';
+        p.innerHTML = options.text;
+        toast.appendChild(p);
+      }
+
+      // icon
+      if (options.icon) {
+        var img = document.createElement('img');
+        img.src = options.icon;
+        img.className = 'cooltoast-icon';
+        toast.appendChild(img);
+      }
+
+      // click callback
+      if (typeof options.callback === 'function') {
+        toast.addEventListener('click', options.callback);
+      }
+
+      // toast api
+      toast.hide = function () {
+        toast.className += ' cooltoast-fadeOut';
+        toast.addEventListener('animationend', removeToast, false);
+      };
+
+      // autohide
+      if (options.timeout) {
+        setTimeout(toast.hide, options.timeout);
+      }
+      // else setTimeout(toast.hide, 2000);
+
+      if (options.type) {
+        toast.className += ' cooltoast-' + options.type;
+      }
+
+      toast.addEventListener('click', toast.hide);
+
+
+      function removeToast() {
+        document.getElementById('cooltoast-container').removeChild(toast);
+      }
+
+      document.getElementById('cooltoast-container').appendChild(toast);
+      return toast;
+
+    }
+  }
+
+  return toast;
+
+});
