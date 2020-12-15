ff4cedbf8e6aed1b08d00ed1897c280816c26646
Giada Rastelli <giada.rastelli1996@gmail.com>
2020-12-15 22:15:02 +0100

    new folder
    p5 sound experiment to implement in the project

---
+++ sound-experiments/sketches/sketch1.js
@@ -0,0 +1,17 @@
+//audio track when page is loaded 
+
+var song;
+
+function setup() {
+    createCanvas(400, 400);
+    song = loadSound("Psychic.mp3", loaded);
+
+}
+
+function loaded() {
+    song.play();
+}
+
+function draw() {
+    background(50);
+}
---
+++ sound-experiments/sketches/sketch2.js
@@ -0,0 +1,17 @@
+// audio track on click 
+
+var song;
+
+function setup() {
+    createCanvas(400, 400);
+    song = loadSound("water effect echo.mp3");
+
+}
+
+function mouseClicked() {
+    song.play();
+}
+
+function draw() {
+    background(50);
+}
